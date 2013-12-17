eef457f252209d2fae87610d4b4b8281cf27b4c7
Jim Moffitt <jmoffitt@gnipcentral.com>
2013-12-17 11:40:35 -0700

    Update StreamingConnection_Async.cs
    Basic Authentication updates...

--- Premium Stream Connection/C#/StreamingConnection_Async.cs
+++ Premium Stream Connection/C#/StreamingConnection_Async.cs
@@ -39,10 +39,14 @@ namespace StreamingConnection
             string username = "ENTER_USERNAME_HERE";
             string password = "ENTER_PASSWORD_HERE";
 
-            //Authentication details.
-            NetworkCredential nc = new NetworkCredential(username, password);
-            request.Credentials = nc;
-            request.PreAuthenticate = true;
+            string authInfo = string.Format("{0}:{1}", username, password);
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            request.Headers.Add("Authorization", "Basic " + authInfo);
+                
+            //In some Windows environments, this alternative Basic Authentication method is available.
+            //NetworkCredential nc = new NetworkCredential(username, password);
+            //request.Credentials = nc;
+            //request.PreAuthenticate = true;
 
             request.Headers.Add("Accept-Encoding", "gzip");
             request.Accept = "application/json";
