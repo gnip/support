d9e720c79f7cf59faae31bc2ab5dc61653a2c373
Jim Moffitt <jmoffitt@gnipcentral.com>
2014-03-13 10:20:37 -0600

    No URL encoding with POST method, other updates.

--- Rules API/Java/AddRule.java
+++ Rules API/Java/AddRule.java
@@ -6,100 +6,99 @@ import java.net.URL;
 
 class AddRule {
   public static void main(String[] args) throws IOException  {
+  	
+    String charset = "UTF-8"; //All things Gnip use UTF-8. 	
 
-        String username = "ENTER_USERNAME_HERE";
-        String password = "ENTER_PASSWORD_HERE";
+    String username = "ENTER_USERNAME_HERE";
+    String password = "ENTER_PASSWORD_HERE";
 
 //	Expected Premium Stream URL Format:
 //	https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json
 	
-	String dataCollectorURL = "ENTER_RULES_API_URL_HERE";
-    String charset = "UTF-8";
-    String param1 = "testTag";  //tag for rule
-    String param2 = "testRule"; //our rule to add
-        
-	
-	String query = String.format("{\"rules\":[{\"tag\":\"%s\",\"value\":\"%s\"}]}",
-             java.net.URLEncoder.encode(param1, charset), java.net.URLEncoder.encode(param2, charset));
+    String rules_api_url = "https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json";
+    String rule_value = "(lang:en OR profile_country_code:us) weather"; //Rule syntax to add.
+    String rule_tag = "weather";  //Tag for rule. Tags are optional, but recommended!
+    String query = String.format("{\"rules\":[{\"value\":\"%s\",\"tag\":\"%s\"}]}",rule_value, rule_tag);
 
-        HttpURLConnection connection = null;
-        InputStream inputStream = null;
+
+    HttpURLConnection connection = null;
+    InputStream inputStream = null;
+    try {
+        connection = getConnection(rules_api_url, username, password);
+
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Accept-Charset", charset);
+        connection.setRequestProperty("Content-Type", "text/json");
+
+        OutputStream output = null;
         try {
-            connection = getConnection(dataCollectorURL, username, password);
+             output = connection.getOutputStream();
+             output.write(query.getBytes(charset));
+        } finally {
+             if (output != null) try { output.close(); } catch (IOException logOrIgnore) {}
+        }
 
-            connection.setDoOutput(true);
-            connection.setRequestProperty("Accept-Charset", charset);
-            connection.setRequestProperty("Content-Type", "text/json");
-
-            OutputStream output = null;
-            try {
-                 output = connection.getOutputStream();
-                 output.write(query.getBytes(charset));
-            } finally {
-                 if (output != null) try { output.close(); } catch (IOException logOrIgnore) {}
-            }
-
-            int responseCode = connection.getResponseCode();
-            String responseMessage = connection.getResponseMessage();
-            if (responseCode >= 200 && responseCode <= 299) {
+        int responseCode = connection.getResponseCode();
+        String responseMessage = connection.getResponseMessage();
+        
+        if (responseCode >= 200 && responseCode <= 299) {
                 
-                inputStream = connection.getInputStream();
+        	inputStream = connection.getInputStream();
 
-                // Just print the first line of the response.
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line = reader.readLine();
-                System.out.println("Response Code: " + responseCode + " -- " + responseMessage);
-		while(line != null){
-                    System.out.println(line);
-                    line = reader.readLine();
-                }
-            } else {
-                 handleNonSuccessResponse(connection);
-                 inputStream = connection.getErrorStream();
-                 BufferedReader reader3 = new BufferedReader(new InputStreamReader(inputStream));
-                 String line2 = reader3.readLine();
-                 while (line2 != null){
-                 	System.out.println(line2);
-                        line2 = reader3.readLine();
-                 }
+            // Just print the first line of the response.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+            System.out.println("Response Code: " + responseCode + " -- " + responseMessage);
+			while(line != null){
+            	System.out.println(line);
+                line = reader.readLine();
+            }
+         } else {
+         	handleNonSuccessResponse(connection);
+            inputStream = connection.getErrorStream();
+            BufferedReader reader3 = new BufferedReader(new InputStreamReader(inputStream));
+            String line2 = reader3.readLine();
+            while (line2 != null){
+            	System.out.println(line2);
+                line2 = reader3.readLine();
             }
         }
+    }
     catch (Exception e) {
             e.printStackTrace();
             if (connection != null) {
             }
-        } finally {
-            if (inputStream != null) {
-                inputStream.close();
-            }
+    } finally {
+    	if (inputStream != null) {
+        	inputStream.close();
         }
+    }
     
   }
 
-      private static void handleNonSuccessResponse(HttpURLConnection connection) throws IOException {
-          int responseCode = connection.getResponseCode();
-          String responseMessage = connection.getResponseMessage();
-          System.out.println("Response Code: " + responseCode + " -- " + responseMessage);
-      }
+  private static void handleNonSuccessResponse(HttpURLConnection connection) throws IOException {
+      int responseCode = connection.getResponseCode();
+      String responseMessage = connection.getResponseMessage();
+      System.out.println("Response Code: " + responseCode + " -- " + responseMessage);
+  }
 
-      private static HttpURLConnection getConnection(String urlString, String username, String password) throws IOException {
-          URL url = new URL(urlString);
+	private static HttpURLConnection getConnection(String urlString, String username, String password) throws IOException {
+    	URL url = new URL(urlString);
 
-          HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-          connection.setRequestMethod("POST");
-          connection.setReadTimeout(1000 * 60 * 60);
-          connection.setConnectTimeout(1000 * 10);
+      	HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    connection.setRequestMethod("POST");
+	    connection.setReadTimeout(1000 * 60 * 60);
+	    connection.setConnectTimeout(1000 * 10);
+	
+	    connection.setRequestProperty("Authorization", createAuthHeader(username, password));
+	
+		return connection;
+	}
 
-          connection.setRequestProperty("Authorization", createAuthHeader(username, password));
-
-           return connection;
-      }
-
-      private static String createAuthHeader(String username, String password) throws UnsupportedEncodingException {
-          BASE64Encoder encoder = new BASE64Encoder();
-          String authToken = username + ":" + password;
-          return "Basic " + encoder.encode(authToken.getBytes());
-      }
-  
+  	private static String createAuthHeader(String username, String password) throws UnsupportedEncodingException {
+    	BASE64Encoder encoder = new BASE64Encoder();
+      	String authToken = username + ":" + password;
+      	return "Basic " + encoder.encode(authToken.getBytes());
+  	}
 }
 
