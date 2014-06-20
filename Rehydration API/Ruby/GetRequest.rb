bc77d60d99b2c390706a49319bf7c9b3b99efa28
Jim Moffitt <jmoffitt@gnipcentral.com>
2014-06-20 13:48:02 -0600

    Using net/https, not curb.

--- Rehydration API/Ruby/GetRequest.rb
+++ Rehydration API/Ruby/GetRequest.rb
@@ -1,25 +1,30 @@
-require 'rubygems'
-require 'curb'
+require "net/https"     #HTTP gem.
+require "uri"
 
-# This uses the 'curb' libcurl wrapper for ruby, found at https://github.com/taf2/curb/  
+# This uses the standard net/https gem.
 # prints data to stdout.
 
 url = "ENTER_API_URL_HERE"
 user = "ENTER_USERNAME_HERE"
 pass = "ENTER_PASSWORD_HERE"
 
-tweetId = '403245346194616320'
+tweetId = '477926086391521280,479442146567544832'
 
-queryString= url + "?ids=" + tweetId
+uri = URI.parse(url)
+uri.query = "ids=" + tweetId
 
-Curl::Easy.http_get queryString do |c|
-  c.http_auth_types = :basic
-  c.username = user
-  c.password = pass
-  c.verbose = true # Modify to false to limit output to only JSON rule payload 
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Get.new(uri.request_uri)
+request.basic_auth(user, pass)
 
-  c.on_body do |data|
-    puts data
-    data.size # required by curl's api.
-  end
+begin
+    response = http.request(request)
+rescue
+    sleep 5
+    response = http.request(request) #try again
 end
+
+#Print out response.
+puts response.body
+
