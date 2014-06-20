82dbcb1b4cea00455c2387d7a40f159833f6a5b1
Jim Moffitt <jmoffitt@gnipcentral.com>
2014-06-20 14:54:23 -0600

    Using net/https, not curb. Using (more) real-world rule.

--- Rules API/Ruby/DeleteRule.rb
+++ Rules API/Ruby/DeleteRule.rb
@@ -1,27 +1,33 @@
-require 'rubygems'
-require 'curb'
+require "net/https"     #HTTP gem.
+require "uri"
 
-# This uses the 'curb' libcurl wrapper for ruby, found at https://github.com/taf2/curb/  
+# This uses the standard 'net/http' gem.
+# prints data to stdout.
 
-# Expected Premium Stream URL Format:
-# https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json
+# Rules API endpoint format:
+#   https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json
 
 url = "ENTER_RULES_API_URL_HERE"
 user = "ENTER_USERNAME_HERE"
 pass = "ENTER_PASSWORD_HERE"
 
-rule_value = "testRule"
-rule = "{\"rules\":[{\"value\":\"" + rule_value + "\"}]}"
+rule_value = "(gnip OR \\\"this exact phrase\\\") country_code:us"
+rule_tag = "my_tag"
 
-Curl::Easy.http_delete(url) do |c|
-  c.http_auth_types = :basic
-  c.username = user
-  c.password = pass
-  c.post_body = rule 
-  c.verbose = true 
+rules_json = "{\"rules\":[{\"value\":\"" + rule_value + "\",\"tag\":\"" + rule_tag + "\"}]}"
 
-  c.on_body do |data|
-    puts data
-    data.size # required by curl's api.
-  end
+uri = URI(url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Delete.new(uri.path)
+request.body = rules_json
+request.basic_auth(user, pass)
+
+begin
+    response = http.request(request)
+rescue
+    sleep 5
+    response = http.request(request) #try again
 end
+
+puts response
