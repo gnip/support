5ef68afe1c915763487eda663a21162032338372
Jim Moffitt <jmoffitt@gnipcentral.com>
2014-06-20 14:14:40 -0600

    Using net/https, not curb.

--- Rules API/Ruby/ListRules.rb
+++ Rules API/Ruby/ListRules.rb
@@ -1,23 +1,28 @@
-require 'rubygems'
-require 'curb'
+require "net/https"     #HTTP gem.
+require "uri"
 
-# This uses the 'curb' libcurl wrapper for ruby, found at https://github.com/taf2/curb/  
+# This uses the standard net/https gem. 
+# prints data to stdout.
 
-# Expected Premium Stream URL Format:
 # https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json
 
 url = "ENTER_RULES_API_URL_HERE"
 user = "ENTER_USERNAME_HERE"
 pass = "ENTER_PASSWORD_HERE"
 
-Curl::Easy.http_get url do |c|
-  c.http_auth_types = :basic
-  c.username = user
-  c.password = pass
-  c.verbose = true # Modify to false to limit output to only JSON rule payload 
+uri = URI.parse(url)
 
-  c.on_body do |data|
-    puts data
-    data.size # required by curl's api.
-  end
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Get.new(uri.request_uri)
+request.basic_auth(user, pass)
+
+begin
+    response = http.request(request)
+rescue
+    sleep 5
+    response = http.request(request) #try again
 end
+
+#Print out response.
+puts response.body
