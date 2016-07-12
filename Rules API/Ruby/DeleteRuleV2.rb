72fed7e7ecff3162231a7d84b1d5dbcffc641c18
Harrison Neff <hneff@twitter.com>
2016-07-12 14:47:59 -0600

    Using net/https, not curb. Using (more) real-world rule.

---
+++ Rules API/Ruby/DeleteRuleV2.rb
@@ -0,0 +1,34 @@
+
+require "net/https"     #HTTP gem.
+require "uri"
+
+# This uses the standard 'net/http' gem.
+# prints data to stdout.
+
+# Rules API endpoint format:
+#   https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json
+
+url = "ENTER_RULES_API_URL_HERE"
+user = "ENTER_USERNAME_HERE"
+pass = "ENTER_PASSWORD_HERE"
+
+rule_value = "(gnip OR \\\"this exact phrase\\\")"
+rule_tag = "my_tag"
+
+rules_json = "{\"rules\":[{\"value\":\"" + rule_value + "\",\"tag\":\"" + rule_tag + "\"}]}"
+
+uri = URI(url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Post.new(uri.path+"?_method=delete")
+request.body = rules_json
+request.basic_auth(user, pass)
+
+begin
+    response = http.request(request)
+rescue
+    sleep 5
+    response = http.request(request) #try again
+end
+
+puts response
