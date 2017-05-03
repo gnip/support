0485b0552eb73f8a4f237dafb665b4707121ab3c
John Demos <jdemos@twitter.com>
2017-05-03 11:50:31 -0600

    JD: Update and simplify PHP rule snippets.

--- Rules API/PHP/ListRules.php
+++ Rules API/PHP/ListRules.php
@@ -1,34 +1,19 @@
 <?php
-
-$user = "ENTER_USERNAME_HERE";
-$pass = "ENTER_PASSWORD_HERE";
-
-//	Expected Premium Stream URL Format:
-//	https://api.gnip.com:443/accounts/<account>/publishers/<publisher>/streams/<stream>/<label>/rules.json
-
-$url = "ENTER_RULES_API_URL_HERE";
-
+// Instantiate an instance of cURL
 $ch   = curl_init($url);
 curl_setopt_array($ch, array(
-  CURLOPT_URL => $url,
-  CURLOPT_ENCODING => "gzip",
+  CURLOPT_RETURNTRANSFER => 1,
+  // Add your account name and stream label to the URL below
+  CURLOPT_URL => "https://gnip-api.twitter.com/rules/powertrack/accounts/{ACCOUNT_NAME}/publishers/twitter/{STEAM_LABEL}.json",
   CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-  CURLOPT_USERPWD => $user.":".$pass,
-//  CURLOPT_VERBOSE => true
-
+  // Enter your username and password (basic auth)
+  CURLOPT_USERPWD => "{USERNAME}".":"."{PASSWORD}",
+  // CURLOPT_VERBOSE => true  // Uncomment for curl verbosity
 ));
+    $response = curl_exec($ch);
+    curl_close($ch);
+print $response."\n";
 
-    $content = curl_exec( $ch );
-    $err     = curl_errno( $ch );
-    $errmsg  = curl_error( $ch );
-    $header  = curl_getinfo( $ch );
-    curl_close( $ch );
 
-    $header['errno']   = $err;
-    $header['errmsg']  = $errmsg;
-    $header['content'] = $content;
-    return $header;
-
-print $content."\n";
 
 ?>
