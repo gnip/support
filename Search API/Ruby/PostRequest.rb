0e482a9f8ab005b1a65bfb90a73b86a5f612690c
@snowman <jimmoffitt@yahoo.com>
2017-09-01 16:46:58 -0600

    Update PostRequest.rb

--- Search API/Ruby/PostRequest.rb
+++ Search API/Ruby/PostRequest.rb
@@ -1,8 +1,5 @@
-require "net/https"     #HTTP gem.
+require "net/https"   # This uses the standard 'net/http' gem.
 require "uri"
-
-# This uses the standard 'net/http' gem.
-# prints data to stdout.
 
 url = "ENTER_API_URL_HERE"
 user = "ENTER_USERNAME_HERE"
@@ -10,7 +7,7 @@ pass = "ENTER_PASSWORD_HERE"
 
 rule = "(snow OR rain) profile_region:co"
 
-queryString = "{\"query\":\"" + rule + "\",\"publisher\":\"twitter\",\"maxResults\":\"100\"}"
+queryString = "{\"query\":\"" + rule + "\",\"maxResults\":\"100\"}"
 
 uri = URI(url)
 http = Net::HTTP.new(uri.host, uri.port)
@@ -26,4 +23,5 @@ rescue
     response = http.request(request) #try again
 end
 
-puts response.body
+puts response.body # prints data to stdout.
+
